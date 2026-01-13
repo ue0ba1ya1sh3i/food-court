b7d7d8f86614046f71444533cbf649a458c0a86f
ue0ba1ya1sh3i <ue0ba1ya1sh3i@gmail.com>
2026-01-13 19:17:52 +0900

    Firebaseのセットアップ改

---
+++ functions/lib/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const firebase_functions_1 = require("firebase-functions");
+// アクセス制限
+(0, firebase_functions_1.setGlobalOptions)({ maxInstances: 10 });
+//# sourceMappingURL=index.js.map
--- src/component/setup.tsx
+++ src/component/setup.tsx
@@ -1,8 +1,17 @@
 import React from "react"
 import { useBackgroundClass } from "../hooks/store/background"
 
+// すべてのhooksのインポート
+const modules = import.meta.glob("../setup/hooks/*.ts", { eager: true })
+
 export default function App({ children }: { children: React.ReactNode }) {
   const { backgroundClass } = useBackgroundClass()
+
+  // 各モジュールのコードをコンポーネント内で実行
+  for (const path in modules) {
+      const module = modules[path] as { default?: () => void }
+      module.default?.()
+  }
 
   return (
     <div className={backgroundClass}>
